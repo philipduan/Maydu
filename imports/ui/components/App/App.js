f88e6809646b80cca3468865422b9c60f42c3bd2
Max Page <maxpage@Maxs-MacBook-Pro.local>
2018-02-20 16:42:05 -0500

    UI for Sign In Route

--- imports/ui/components/App/App.js
+++ imports/ui/components/App/App.js
@@ -2,7 +2,20 @@ import React from 'react';
 import './styles.css';
 
 const Layout = () => {
-  return <div> hi </div>;
+  return (
+    <div className="Login-Container">
+      <div class="Login-Box">
+        <h3> MayDu </h3>
+        <div class="Login-Box-Inputs">
+          <input type="email" placeholder="Email" className="Email-Input" />
+          <input type="password" placeholder="Password" className="Password-Input" />
+        </div>
+        <button type="submit" className="Sign-In-Submit"> Sign In </button>
+        <p> Don't have an account? </p>
+        <button type="submit" className="Create-Account-Submit"> Create An Account </button>
+      </div>
+    </div>
+  );
 };
 
 export default Layout;
