72d0c9104f4e16bf47d114300924dc630a3e2fa8
philipduan <philip_du_an@hotmail.com>
2018-02-21 10:33:59 -0500

    Making sure remote phil branch working

--- imports/api/Sessions.js
+++ imports/api/Sessions.js
@@ -2,23 +2,25 @@ import { Mongo } from 'meteor/mongo';
 export const Sessions = new Mongo.Collection('sessions');
 
 Sessions.schema = new SimpleSchema({
-    sessionCreator: { type: String, optional: false },
-    title: { type: String, optional: false },
-    school: { type: String, optional: false },
-    courseCode: { type: Number, defaultValue: null, optional: false },
-    date: { type: String, optional: false },
-    time: { type: String, optional: false },
-    location: { type: String, optional: false },
-    description: { type: String, optional: false },
-    // rsvpAttending: { type: [UsersSchema] },
-    // rsvpPending: { type: [UsersSchema] },
-    maxCapacity: { type: Number, defaultValue: null, optional: false },
+  sessionCreator: { type: String, optional: false },
+  title: { type: String, optional: false },
+  school: { type: String, optional: false },
+  courseCode: { type: Number, defaultValue: null, optional: false },
+  date: { type: String, optional: false },
+  time: { type: String, optional: false },
+  location: { type: String, optional: false },
+  description: { type: String, optional: false },
+  // rsvpAttending: { type: [UsersSchema] },
+  // rsvpPending: { type: [UsersSchema] },
+  maxCapacity: { type: Number, defaultValue: null, optional: false }
 });
 
-if (Meteor.isServer) { //Limiting and publishing/giving data to client
-    Meteor.publish('sessions', function sessionsPublications() {
-        const loggedInUser = Meteor.users.find(this.userId);
-        return Sessions.find({ school: loggedInUser.school });
-    });
+if (Meteor.isServer) {
+  //Limiting and publishing/giving data to client
+  Meteor.publish('sessions', function sessionsPublications() {
+    const loggedInUser = Meteor.users.find(this.userId);
+    return Sessions.find({ school: loggedInUser.school });
+  });
 }
 
+//test
