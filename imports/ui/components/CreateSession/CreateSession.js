8047391a10c5d077c98a18e9a71403932537e1cf
philipduan <philip_du_an@hotmail.com>
2018-02-21 16:11:32 -0500

    Set up partial create session form

--- imports/ui/components/CreateSession/CreateSession.js
+++ imports/ui/components/CreateSession/CreateSession.js
@@ -5,9 +5,79 @@ import './styles.css';
 class CreateSession extends Component {
   constructor() {
     super();
+    this.state = {
+      email: '',
+      password: '',
+      error: ''
+    };
   }
+
+  //SignIn Methods
+
+  //If user does not have an account - this takes them to SignUp form
+  createAnAccount = () => {
+    <Link to={`/SignUp`} />;
+    alert('i work');
+  };
+
+  //Setting state of user email and passworld on each keystroke
+  handleEmailAndPasswordState = event => {
+    event.preventDefault();
+    this.setState({
+      email: this.emailInput.value,
+      password: this.passwordInput.value
+    });
+  };
+
+  //Checking DB if the user and password match - if no user exists or their password is inncorect, throw an error
+  handleSignInSubmit = event => {
+    event.preventDefault();
+    Meteor.loginWithPassword(this.state.email, this.state.password, err => {
+      this.setState({ error: `${err.reason}, please try again!` });
+    });
+    Meteor.loggingIn();
+  };
+
   render() {
-    return <CreateSessionFields />;
+    return (
+      <div className="Create-Session-Container">
+        <div className="Create-Session-Box">
+          <h3> Create A Study Session </h3>
+          <form onSubmit={this.handleSignInSubmit}>
+            <input
+              onChange={this.handleEmailAndPasswordState}
+              ref={input => (this.emailInput = input)}
+              type="email"
+              placeholder="Email Address"
+              className="Email-Input"
+            />
+            <input
+              onChange={this.handleEmailAndPasswordState}
+              ref={input => (this.passwordInput = input)}
+              type="password"
+              placeholder="Password"
+              className="Password-Input"
+            />
+            <p> {this.state.error} </p>
+            <button type="submit" className="Sign-In-Submit">
+              {' '}
+              Sign In{' '}
+            </button>
+          </form>
+          <div className="Create-Account-SignIn">
+            <p> Don't have an account? </p>
+            <button
+              onClick={this.createAnAccount}
+              type="submit"
+              className="Create-Account-Submit"
+            >
+              {' '}
+              Create An Account{' '}
+            </button>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
