1579a3695230affc5233500d2d67ae99c6cfb6be
Riley MacDonald <riley@Rileys-MacBook-Air.local>
2018-02-26 15:28:50 -0500

    base css card layout done

--- imports/ui/components/SessionFeed/SessionCard.js
+++ imports/ui/components/SessionFeed/SessionCard.js
@@ -11,34 +11,57 @@ import FlatButton from 'material-ui/FlatButton';
 import './style.css';
 import { withRouter } from 'react-router-dom';
 
+
+
 const SessionCard = ({ data, history }) => (
-  <div className="sessionCard">
-    {/* {console.log('data', data)} */}
-    <Card>
-      {/* <img src={data.imageurl} alt={data.title} /> */}
-      <CardHeader
-        style={{ cursor: 'pointer' }}
-        onClick={() => history.push(`/user/${data.sessionCreator._id}`)}
-        title={`Creator: ${data.sessionCreator.profile.fullName}`}
-        subtitle={data.sessionCreator.profile.bio}
-        // avatar={
-        //   <Gravatar email={data.itemowner.email} className="GravatarImg" />
-        // }
-      />
-      <CardTitle title={data.title} subtitle={data.courseCode} />
-      <CardText>{data.intersection}</CardText>
-      <CardText>{data.institution}</CardText>
-      <CardActions>
-        {data ? <FlatButton className="join-btn" label="Join" /> : ''}
-      </CardActions>
-    </Card>
+  <div className="session-brief-contain">
+  <header className="session-brief-header">
+    CS350 World of Code
+  </header>
+  <h2 className="session-brief-date">Date: tues, march 11</h2>
+  <h2 className="session-brief-time">Time: 4:30pm</h2>
+  <p className="session-brief-bio">I'm going to lap some water out of my master's cup meow lick human with sandpaper tongue. Throwup on your pillow climb a tree, wait for a fireman jump to </p>
+  <div className="btn-contain">
+    <button className="session-brief-moreInfo">More Info</button>
+    <button className="rsvp">RSVP</button>
   </div>
+</div>
+  
 );
 
 export default withRouter(SessionCard);
 
 // Old Card Components
 //====================
+
+
+
+// <div className="sessionCard">
+//     {/* {console.log('data', data)} */}
+//     <Card>
+//       {/* <img src={data.imageurl} alt={data.title} /> */}
+//       <CardHeader
+//         style={{ cursor: 'pointer' }}
+//         onClick={() => history.push(`/user/${data.sessionCreator._id}`)}
+//         title={`Creator: ${data.sessionCreator.profile.fullName}`}
+//         subtitle={data.sessionCreator.profile.bio}
+//         // avatar={
+//         //   <Gravatar email={data.itemowner.email} className="GravatarImg" />
+//         // }
+//       />
+//       <CardTitle title={data.title} subtitle={data.courseCode} />
+//       <CardText>{data.intersection}</CardText>
+//       <CardText>{data.institution}</CardText>
+//       <CardActions>
+//         {data ? <FlatButton className="join-btn" label="Join" /> : ''}
+//       </CardActions>
+//     </Card>
+//   </div>
+
+
+
+
+
 
 // <CardMedia
 //         overlay={
