411676a1f2d97c606402a037c4f9d5cdecf6ccf9
aseelaldallal <aseelaldallal@gmail.com>
2018-03-03 17:31:47 -0500

    image upload styling

--- imports/ui/components/ImageUpload/ImageUpload.js
+++ imports/ui/components/ImageUpload/ImageUpload.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './styles.css';
 
-const ImageUpload = props => {
-  return (
-    <div className="imageUpload">
-      <label> {props.label} </label>
-      {props.error ? <p className="error">{props.error}</p> : null}
-      <img className="image" src={props.imgURL} />
-      <input type="file" onChange={props.changed} />
-    </div>
-  );
+class ImageUpload extends Component {
+
+  handleFileSelect = (e) => {
+    e.preventDefault();
+    this.refs.fileSelect.click();
+  }
+
+  render() {
+    console.log("REFS: ", this.refs);
+    return (
+      <div>
+        <label className="signup_label"> {this.props.label} </label>
+        {this.props.error ? <p className="signup_error">{this.props.error}</p> : null}
+        <img className="image" src={this.props.imgURL} />
+        <input type="file" onChange={this.props.changed} hidden ref="fileSelect"/>
+        <button className="image_upload_button" onClick={this.handleFileSelect}> Upload A Photo</button>
+      </div>
+    );
+  }
 };
 
 export default ImageUpload;
