8c6b4898f72bc9d9d780bf0645d5d7a22320da27
Philip Du <philip_du_an@hotmail.com>
2018-03-04 15:00:39 -0500

    Id of new session created by the user now saved in the user document, createdSessions field

--- client/modules/upload-to-amazon-s3.js
+++ client/modules/upload-to-amazon-s3.js
@@ -1,17 +1,12 @@
+// let upload = file => {
+//     const uploader = new Slingshot.Upload("uploadToAmazonS3");
+//     uploader.send(file, (error, url) => {
+//         if(error) {
+//             console.log("Error uploading to S3: ", error);
+//         } else {
+//             Meteor.users.update(Meteor.userId(), {imageURL: url});
+//         }
+//     })
+// }
 
-
-let upload = file => {
-    const uploader = new Slingshot.Upload("uploadToAmazonS3");
-    uploader.send(file, (error, url) => {
-        if(error) {
-            console.log("Error uploading to S3: ", error);
-        } else {
-            Meteor.users.update(Meteor.userId(), {imageURL: url});
-        }
-    })
-}
-
-
-
-
-Modules.client.uploadToAmazonS3 = upload;
+// Modules.client.uploadToAmazonS3 = upload;
